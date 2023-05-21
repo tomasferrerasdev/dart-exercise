1c0d1880324f54e4f64bf2e1d402fd4a4983453b
Tomas Ferreras <hellotomasdev@gmail.com>
2023-05-21 16:19:00 -0300

    add getters and setters

--- 10_getters_setters.dart
+++ 10_getters_setters.dart
@@ -23,3 +23,33 @@ class Square {
   
   double calculateArea() => _side * _side;
 }
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+void main() {
+  final mySquare = Square(side: -10);
+  //mySquare.side = -5; 
+  print('Area: ${mySquare.area}');
+}
+
+
+class Square {
+  double _side;
+  
+  Square({ required double side})
+    : assert(side >= 0, 'Side must be greater or equal than zero'),
+      _side = side;
+  
+  double get area {
+    return _side * _side;
+  }
+  
+  set side(double val){
+    print('Set new value: $val');
+    if(val < 0) throw 'Value must be greater than zero';
+    _side = val;
+  }
+  
+  double calculateArea() => _side * _side;
+}
